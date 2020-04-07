bec221c6cbf502f03cebc5d3f9bece0981793729
Antonio Hernandez Souza <ahernandezsouza@gmail.com>
2020-04-06 23:31:22 -0500

    Add Container

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build /home/jjahs/projects/devops/devops-capstone -t jjahs/capstone:latest
+docker build ~/project -t jjahs/capstone:latest
 # Step 2:
 # List docker images
 docker image ls jjahs/capstone:latest
